546b9a2e73381371ce7241ce8e1bedae6ea08d08
ksenias <kseniatsn@gmail.com>
2025-05-06 17:56:07 +0300

    add footer & footerlistblock

---
+++ src/components/common/BlockAbout/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="footer">123</div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style lang="postcss">
+.footer {
+    background-color: var(--color-black);
+    height: 565px;
+}
+</style>
---
+++ src/components/ui/BaseButton/FooterListBlock.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul>
+        <li><a href="/...">...</a></li>
+    </ul>
+</template>
+
+<script>
+export default {
+  name: 'FooterListBlock',
+}
+</script>
+
+<style lang="postcss">
+
+</style>
