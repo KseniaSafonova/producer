c11fcbc17d1203f01766041557ced16f1802a2a0
ksenias <kseniatsn@gmail.com>
2025-05-09 16:09:35 +0300

    блоки и стили футера

--- src/components/common/Footer/Footer.vue
+++ src/components/common/Footer/Footer.vue
@@ -8,25 +8,51 @@ export default {
 
 <template>
   <div class="footer">
-    <div class="footerContactsBlock">
-      <p>+375293725243</p>
-      <p>info@glebpetrov.com</p>
-      <p>ИП Петров Глеб Владимирович</p>
-      <p>Copyright @ 2025 Gleb Petrov. All Right Reserved</p>
-      <p>
-        <img src="../../../assets/img/instagram.png" alt="">
-        <img src="../../../assets/img/telegram.png" alt="">
-      </p>
+    <div class="logo">LOGO</div>
+      <div class="mainFooterBlock">
+      <div class="footerContactsBlock">
+        <div>+375293725243</div>
+        <div><a href="">info@glebpetrov.com</a></div>
+        <div>ИП Петров Глеб Владимирович</div>
+        <div class="socialNetworks">
+          <img src="../../../assets/img/instagram.png" alt="">
+          <a href="https://t.me/magistr_glebus">
+          <img src="../../../assets/img/telegram.png" alt="">
+          </a> 
+        </div>
+      </div>
+    <FooterListBlock/>
     </div>
-   <FooterListBlock/>
-   </div>
+  <div class="copyright">Copyright @ 2025 Gleb Petrov. All Right Reserved</div>
+  </div>
 </template>
 
 <style lang="postcss">
 .footer {
     background-color: var(--color-black);
     height: 565px;
-    height: 200px;
+    display: grid;  
+}
+
+.mainFooterBlock {
     display: flex;
+  margin-top: 200px;
+}
+
+.footerContactsBlock {
+  width: 413px;
+  color: var(--color-gray-30);
+  margin-left: 80px;
+}
+
+.copyright {
+  color: var(--color-gray);
+  margin-top: 190px;
+  margin-left: 80px;
+}
+
+.logo {
+  margin-left: 80px;
+  color: white;
 }
 </style>
--- src/components/ui/FooterListBlock/FooterListBlock.vue
+++ src/components/ui/FooterListBlock/FooterListBlock.vue
@@ -28,37 +28,41 @@ export default {
 
 <template>
     <ul>
-      <div>Условия и политика</div>
+      <div class="listHeader">Условия и политика</div>
         <li v-for="condition in conditions">
         <a href="">{{ condition.text }}</a>  
         </li>
     </ul>
     <ul>
-      <div>Gleb Petrov</div>
+      <div class="listHeader">Gleb Petrov</div>
         <li v-for="item in main">
           <a href="">{{ item.text }}</a>
         </li>
     </ul>
     <ul>
-      <div>Услуги</div>
+      <div class="listHeader">Услуги</div>
         <li v-for="service in services">
           <a href="">{{ service.text }}</a>
         </li>
     </ul>
-   
 </template>
 
 <style lang="postcss">
 a {
 text-decoration: none;
+font-size: 15px;
 }
 
 ul {
   list-style-type: none;
   color: var(--color-gray);
+  width: 271px;
 }
 
-div {
-  color: var( --color-gray-30);
+.listHeader {
+  color: var(--color-white);
+  height: 44px;
+  font-weight: 500;
+  font-size: 18px;
 }
 </style>
