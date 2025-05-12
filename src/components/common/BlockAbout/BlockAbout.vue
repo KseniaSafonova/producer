dfb3ea851fb4e1fa71c4e88dedc440d9b0192dec
Hellga Prokopovich <hellgamur@mail.ru>
2025-05-12 22:04:50 +0300

    feat: BaseTitlesContainer & BlockAbout (#5)
    * feat: BaseTitlesContainer & BlockAbout
    
    * upgraded classes for headings in BaseTitlesContainer.vue
    
    * upgraded according to comments in PR: changed order of headers names, covered BlockAbout inside component BaseContainer, added [] to BaseTitlesContainer

--- src/components/common/BlockAbout/BlockAbout.vue
+++ src/components/common/BlockAbout/BlockAbout.vue
@@ -1,14 +1,25 @@
 <template>
-  <div class="block-about"></div>
+  <!-- <div class="block-about"></div>
+  </div> -->
+  <BaseContainer>
+    <BaseTitlesContainer
+        title="Уникален не я, а мой опыт"
+        subtitle="Обо мне"
+        align="left"
+      />
+      <div>Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. О текстов, языком проектах грустный свой безорфографичный дал взобравшись на берегу своих ручеек свою, то возвращайся пунктуация! Вершину текст жизни наш!</div>
+  </BaseContainer>
 </template>
 
 <script>
 export default {
   name: 'BlockAbout',
 }
+
 </script>
 
-<style lang="postcss">
-.block-about {
-}
+<style>
+/* .block-about {
+  border: red;
+} */
 </style>
---
+++ src/components/ui/BaseTitlesContainer/BaseTitlesContainer.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- <Component
+    :is="div"
+    class="titles-container"
+    :class="[alignTitlesContClass]"
+  ></Component> -->
+
+  <div :class="['titles-container', alignTitlesContClass]">
+    <h2 class="heading-thirdly text-gray">[{{ subtitle }}]</h2>
+    <h3 class="heading-secondary">{{ title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseTitlesContainer',
+  props: {
+    title: String,
+    subtitle: String,
+    align: {
+      type: String,
+      default: 'left',
+      validator: (value) => ['left', 'right'].includes(value),
+    }
+  },
+  computed: {
+    alignTitlesContClass() {
+      return this.align ===  'right'
+      ? 'titles-container_align-right'
+      : 'titles-container_align-left'
+    }
+  }
+}
+</script>
+
+<style>
+.titles-container {
+  margin-bottom: 28px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+.titles-container_align-left {
+  text-align: left;
+}
+.titles-container_align-right {
+  text-align: right;
+}
+</style>
