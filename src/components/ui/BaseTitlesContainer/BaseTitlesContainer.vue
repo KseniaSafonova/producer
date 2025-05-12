e484a93e0a4eadde56165d8c5125d023696cbbf8
Polina Makarova <45349348+sunnyfur@users.noreply.github.com>
2025-05-13 00:15:21 +0500

    Task edits (#8)
    * fix: path of container
    
    * fix: font path
    
    * refactor: styles and linting code
    
    ---------
    
    Co-authored-by: Polina Makarova <sunnyfur9@gmail.com>

--- src/components/ui/BaseTitlesContainer/BaseTitlesContainer.vue
+++ src/components/ui/BaseTitlesContainer/BaseTitlesContainer.vue
@@ -5,31 +5,37 @@
     :class="[alignTitlesContClass]"
   ></Component> -->
 
-  <div :class="['titles-container', alignTitlesContClass]">
-    <h2 class="heading-thirdly text-gray">[{{ subtitle }}]</h2>
-    <h3 class="heading-secondary">{{ title }}</h3>
+  <div class="titles-container" :class="[alignTitlesContClass]">
+    <h2 class="heading-thirdly text-gray">[{{ title }}]</h2>
+    <h3 class="heading-secondary">{{ subtitle }}</h3>
   </div>
 </template>
 
 <script>
 export default {
   name: 'BaseTitlesContainer',
+
   props: {
-    title: String,
-    subtitle: String,
+    title: {
+      type: String,
+    },
+
+    subtitle: {
+      type: String,
+    },
+
     align: {
       type: String,
       default: 'left',
       validator: (value) => ['left', 'right'].includes(value),
-    }
+    },
   },
+
   computed: {
     alignTitlesContClass() {
-      return this.align ===  'right'
-      ? 'titles-container_align-right'
-      : 'titles-container_align-left'
-    }
-  }
+      return this.align === 'right' ? 'titles-container_align-right' : 'titles-container_align-left'
+    },
+  },
 }
 </script>
 
@@ -40,9 +46,11 @@ export default {
   flex-direction: column;
   gap: 8px;
 }
+
 .titles-container_align-left {
   text-align: left;
 }
+
 .titles-container_align-right {
   text-align: right;
 }
